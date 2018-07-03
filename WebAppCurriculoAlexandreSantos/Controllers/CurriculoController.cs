22fedb851638c1a34814d39c47b1cb2be279bb88
Alexandre <pcj92@hotmail.com>
2018-07-03 02:35:09 -0300

    Add project files.

---
+++ WebAppCurriculoAlexandreSantos/Controllers/CurriculoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppCurriculoAlexandreSantos.Models;
+
+namespace WebAppCurriculoAlexandreSantos.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View(new Curriculo());
+        }
+        [HttpPost]
+        public IActionResult Cadastro(Curriculo curriculo)
+        {
+            return View(Curriculo);
+        }
+
+    }
+}
---
+++ WebAppCurriculoAlexandreSantos/Models/Curriculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurriculoAlexandreSantos.Models
+{
+    public class Curriculo
+    {
+        //Infromações Pessoais
+        public int CurriculoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public decimal Altura { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public Sexo Sexo { get; set; }
+
+        // Endereço
+        public string Endereco { get; set; }
+        public string CEP { get; set; }
+
+        //Contato
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+
+        //Conhecimentos
+        public bool HTML { get; set; }
+        public bool CSS { get; set; }
+        public bool JavaScript { get; set; }
+        public string MaisConhecimentos { get; set; }
+
+    }
+}
